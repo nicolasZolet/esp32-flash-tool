f8fe183e19fa472a1e686f9b6462adf78c1aba13
nicolasZolet <nicolas.zolet@gmail.com>
2024-03-20 08:57:38 -0300

    keeping update

--- demo/src/main/java/com/nicolas/demo/HelloApplication.java
+++ demo/src/main/java/com/nicolas/demo/HelloApplication.java
@@ -1,24 +1,10 @@
 package com.nicolas.demo;
 
-import com.nicolas.demo.classes.Cmd;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.stage.Popup;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
-
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.List;
-
-import static javafx.scene.layout.Region.USE_PREF_SIZE;
 
 public class HelloApplication extends Application {
 
@@ -31,21 +17,21 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
 
         HelloApplication.stage = stage;
-
+//
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WINDOW_WIDTH, WINDOW_HEIGHT);
-
+//
         stage.setMinWidth(WINDOW_WIDTH);
         stage.setMinHeight(WINDOW_HEIGHT);
 
         stage.setTitle("ESP32 Flash Tool");
         stage.setScene(scene);
         stage.show();
-
-        stage.addEventFilter(WindowEvent.WINDOW_CLOSE_REQUEST, e->{
-            System.out.println("Close program");
-            windowIsOpen = false;
-        });
+//
+//        stage.addEventFilter(WindowEvent.WINDOW_CLOSE_REQUEST, e->{
+//            System.out.println("Close program");
+//            windowIsOpen = false;
+//        });
 
     }
 
